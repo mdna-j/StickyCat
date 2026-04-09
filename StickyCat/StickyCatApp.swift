7c00493d700119cec3241410435e23ce89a605e8
mdna-j <medin277@csusm.edu>
2026-04-08 21:15:42 -0700

    Initial Commit

---
+++ StickyCat/StickyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyCatApp.swift
+//  StickyCat
+//
+//  Created by Jose  Medina on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct StickyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
